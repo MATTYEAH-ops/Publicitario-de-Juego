826b27ce4febcaba2267956148f5f1a6f1dd4c39
MATTYEAH-ops <lpze.matiasdario.mercier.re@unifranz.edu.bo>
2025-11-11 19:51:33 -0400

    Desarrollo basico del menu

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import Xbox360Menu from './components/Xbox360Menu';
+
+function App() {
+  return (
+    <div className="App">
+      <Xbox360Menu />
+    </div>
+  );
+}
+
+export default App;
